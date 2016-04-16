b169ebf04535653d824e45336c2c0d54f9d488fa
Ahmad EL Masri <ahmadjelmasri@gmail.com>
2016-04-17 01:16:38 +0300

    feat: Add Enemies class to generate new enemy

--- js/app.js
+++ js/app.js
@@ -1,136 +1,144 @@
 var allEnemies = [];
 
 var constants = {
-	PLAYER_MOVEMENT: 82,
-	PLAYER_MOVEMENT_X: 101,
 	MAX_Y_BORDER: 400,
 	MIN_Y_BORDER: 72,
 	MAX_X_BORDER: 404,
 	MIN_X_BORDER: 0,
-	ENEMY_Y_POSITION: [225, 140, 60],
+	PLAYER_MOVEMENT: 82,
+	PLAYER_MOVEMENT_X: 101,
 	PLAYER_START_POINT: [0,400],
-	PLAYER_RESET_POINT: [202, 400]
+	PLAYER_RESET_POINT: [202, 400],
+	STARTINGLIVES: 3,
+	ENEMY_Y_POSITION: [225, 140, 60]
 };
 
 // Enemies our player must avoid
-var Enemy = function(positionY, enemySpeed) {
-	// Variables applied to each of our instances go here,
-	// we've provided one for you to get started
-
-	// The image/sprite for our enemies, this uses
-	// a helper we've provided to easily load images
+var Enemy = function() {
 	this.sprite = 'images/enemy-bug.png';
-	this.x = generateRandomPosition(-1000, -100);
-	this.y = positionY;
-	this.speed = enemySpeed;
+	this.x = generateRandomNumber(100, -10);
+	this.y = constants.ENEMY_Y_POSITION[generateRandomNumber(0, 3)];
+	this.speed = generateRandomNumber(1, 150);
 };
 
-// Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-	// You should multiply any movement by the dt parameter
-	// which will ensure the game runs at the same speed for
-	// all computers.
 	this.x = this.x + this.speed * dt
 	if(this.x > constants.MAX_Y_BORDER) {
-		this.x = generateRandomPosition(-2000, -100);
+		this.x = generateRandomNumber(100, -10);
 	}
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
 	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+var Enemies = function() {
+	this.numberOfEnemies = (player.score + 1) * 3
+};
+
+Enemies.prototype.generateEnemies = function() {
+	if(allEnemies.length < 10) {
+		for(var i = 0 ; i < this.numberOfEnemies; i++) {
+			allEnemies.push(new Enemy());
+		}
+	}
+};
+
+// Our player class
 var Player = function() {
 	this.sprite = 'images/char-boy.png';
 	this.x = constants.PLAYER_START_POINT[0];
 	this.y = constants.PLAYER_START_POINT[1];
 	this.score = 0;
-}
+	this.lives = constants.STARTINGLIVES;
+};
 
 Player.prototype.update = function() {
 	this.xNow = this.x;
 	this.yNow = this.y;
-	this.checkCollisions();
-}
+	this.checkCollisions(this);
+};
 
 Player.prototype.render = function() {
 	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 Player.prototype.hit = function () {
-	player.x = constants.PLAYER_RESET_POINT[0];
-	player.y = constants.PLAYER_RESET_POINT[1];
-}
+	this.x = constants.PLAYER_RESET_POINT[0];
+	this.y = constants.PLAYER_RESET_POINT[1];
+	this.lives == 0 ?  this.gameOver(): this.lives--;
+	this.updateDashboard();
+};
 
-Player.prototype.win = function () {4
-	console.log("win");
-	player.x = 0;
-	player.y = 400;
+Player.prototype.win = function () {
+	this.x = 0;
+	this.y = 400;
+	this.score ++;
+	enemies.generateEnemies();
+	this.updateDashboard();
+};
 
+Player.prototype.gameOver = function() {
+	this.score = 0;
+	this.lives = 3;
+	allEnemies = [];
+	enemies.generateEnemies();
+};
+
+Player.prototype.updateDashboard = function() {
 	ctx.font = "30px Arial";
-	ctx.clearRect(0,0,400,400);
-	ctx.fillText("Score: " + ++this.score ,0,40);
-}
+	ctx.clearRect(0,0,500,500);
+	ctx.fillText("Score: " +this.score,10,40);
+	ctx.fillText(" Lives: " + this.lives ,370,40);
+};
 
-Player.prototype.checkCollisions = function() {
-	 if(player.y < 318 && player.y > 71){
-	 	allEnemies.forEach(function(enemy) {
-	 		var xCondition = player.x -70 < enemy.x &&  enemy.x < player.x + 85,
-	 			yCondition = player.y -15 < enemy.y &&  enemy.y< player.y + 15;
-	 		if(xCondition && yCondition ) {
-				player.hit();
-				console.log("collision");
-	 		}
-	 	});
-	 }else if(player.y < 71) {
-	 	player.win();
-	 }
-}
+Player.prototype.checkCollisions = function(ply) {
+	if(ply.y < 318 && ply.y > 71){
+		allEnemies.forEach(function(enemy) {
+			var xCondition = ply.x -70 < enemy.x &&  enemy.x < ply.x + 85,
+			yCondition = ply.y -15 < enemy.y &&  enemy.y< ply.y + 15;
+			if(xCondition && yCondition) {
+				ply.hit();
+			}
+		});
+	}else if(ply.y < 71) {
+		ply.win();
+	}
+};
 
 Player.prototype.handleInput = function(key) {
 	if(key === 'left') {
-			/* Allow the player to move. when he
-			* is within the canvas boundary
-			*/
-			if (this.x > constants.MIN_X_BORDER) {
-				this.x = this.xNow - constants.PLAYER_MOVEMENT_X;
-			}
-		}
-		if(key === 'up') {
-			if(this.y >= constants.MIN_Y_BORDER){
-				this.y = this.yNow - constants.PLAYER_MOVEMENT;
-			}
-		}
-		if(key === 'right' ) {
-			if(this.x < constants.MAX_X_BORDER) {
-				this.x = this.xNow + constants.PLAYER_MOVEMENT_X;
-			}
-		}
-		if(key === 'down') {
-			if(this.y < constants.MAX_Y_BORDER ) {
-				this.y = this.yNow + constants.PLAYER_MOVEMENT;
-			}
+		/**
+		* Allow the player to move. when he
+		* is within the canvas boundary
+		*/
+		if (this.x > constants.MIN_X_BORDER) {
+			this.x = this.xNow - constants.PLAYER_MOVEMENT_X;
 		}
 	}
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-allEnemies = [new Enemy(60, 100), new Enemy(140, 300), new Enemy(225, 100)];
+	if(key === 'up') {
+		if(this.y >= constants.MIN_Y_BORDER){
+			this.y = this.yNow - constants.PLAYER_MOVEMENT;
+		}
+	}
+	if(key === 'right' ) {
+		if(this.x < constants.MAX_X_BORDER) {
+			this.x = this.xNow + constants.PLAYER_MOVEMENT_X;
+		}
+	}
+	if(key === 'down') {
+		if(this.y < constants.MAX_Y_BORDER ) {
+			this.y = this.yNow + constants.PLAYER_MOVEMENT;
+		}
+	}
+};
 
 var player = new Player();
+var enemies = new Enemies();
+enemies.generateEnemies();
 
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
 	var allowedKeys = {
 		37: 'left',
@@ -142,7 +150,7 @@ document.addEventListener('keyup', function(e) {
 	player.handleInput(allowedKeys[e.keyCode]);
 });
 
-
-function generateRandomPosition(min, max) {
-	return Math.floor(Math.random() * (max - min + 1) + min);
+function generateRandomNumber(a, b) {
+	// Generate Random Number
+	return Math.floor((Math.random() + a)*b);
 }
